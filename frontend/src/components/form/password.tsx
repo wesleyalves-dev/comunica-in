bfc7de12bc5135880682280ead80f3c3408fa7e7
Wesley Alves <wesley_alv@outlook.com>
2025-01-26 09:41:32 -0300

    refactor: refatorado o componente password para permitir mostrar a senha

--- frontend/src/components/form/password.tsx
+++ frontend/src/components/form/password.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useFormContext } from "react-hook-form";
 
 import { FieldError } from "./field-error";
@@ -9,15 +10,28 @@ interface TextInputProps {
 
 export function Password({ name, placeholder }: TextInputProps) {
   const { register } = useFormContext();
+  const [type, setType] = useState("password");
+
+  function togglePassword() {
+    if (type === "password") setType("text");
+    else setType("password");
+  }
 
   return (
     <div>
-      <input
-        placeholder={placeholder}
-        type="password"
-        className="dui-input dui-input-bordered w-full"
-        {...register(name)}
-      />
+      <label className="dui-input dui-input-bordered grid grid-cols-7 items-center justify-between gap-2">
+        <input
+          className="col-span-6"
+          placeholder={placeholder}
+          type={type}
+          {...register(name)}
+        />
+        <button className="col-span-1" type="button" onClick={togglePassword}>
+          <span className="select-none">
+            {type === "password" ? "Mostrar" : "Ocultar"}
+          </span>
+        </button>
+      </label>
       <FieldError name={name} />
     </div>
   );
