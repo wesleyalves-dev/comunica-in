fdd913ea5bed42b580d9762b0548aadc05355c75
Wesley Alves <wesley_alv@outlook.com>
2025-01-26 10:01:13 -0300

    refactor: refatorado create user modal

--- frontend/src/app/(private)/usuarios/components/create-user-modal.tsx
+++ frontend/src/app/(private)/usuarios/components/create-user-modal.tsx
@@ -1,13 +1,9 @@
 import { z } from "zod";
 import { toast } from "react-toastify";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
-import {
-  Form,
-  TextInput,
-  Password,
-  Submit,
-  type FormOnSubmitOptions,
-} from "@/components/form";
+import { Form, TextInput, Password, Submit } from "@/components/form";
 import { closeModal, Modal } from "@/components/modal";
 import { useCreateUser } from "@/hooks/use-create-user";
 import { createUserSchema } from "./schemas";
@@ -21,23 +17,29 @@ export function CreateUserModal() {
     onError: (error: any) =>
       toast.error(error.response?.data.message || "Erro ao efetuar cadastro"),
   });
+  const methods = useForm({
+    resolver: zodResolver(createUserSchema),
+    mode: "onBlur",
+  });
 
-  function handleSubmit(
-    values: z.infer<typeof createUserSchema>,
-    options: FormOnSubmitOptions
-  ) {
+  function handleClose() {
+    methods.reset();
+  }
+
+  function handleSubmit(values: z.infer<typeof createUserSchema>) {
     const { name, username, password } = values;
-    createUser(
-      { name, username, password },
-      { onSuccess: () => options.reset() }
-    );
+    createUser({ name, username, password });
   }
 
   return (
-    <Modal id="create-user-modal">
+    <Modal id="create-user-modal" onClose={handleClose}>
       <h3 className="font-bold text-lg pb-4 select-none">Cadastrar usuário</h3>
 
-      <Form schema={createUserSchema} onSubmit={handleSubmit}>
+      <Form
+        schema={createUserSchema}
+        customMethods={methods}
+        onSubmit={handleSubmit}
+      >
         <TextInput placeholder="Nome" name="name" />
         <TextInput placeholder="Usuário" name="username" />
         <Password placeholder="Senha" name="password" />
--- frontend/src/components/form/form.tsx
+++ frontend/src/components/form/form.tsx
@@ -1,4 +1,4 @@
-import { FormProvider, useForm } from "react-hook-form";
+import { FormProvider, useForm, type UseFormReturn } from "react-hook-form";
 import type { ZodEffects, ZodObject } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
@@ -7,18 +7,26 @@ export interface FormOnSubmitOptions {
 }
 
 interface FormProps {
+  customMethods?: UseFormReturn;
   children?: React.ReactNode;
   schema?: ZodObject<any> | ZodEffects<any>;
   values?: any;
   onSubmit: (data: any, options: FormOnSubmitOptions) => void;
 }
 
-export function Form({ children, schema, values, onSubmit }: FormProps) {
-  const methods = useForm({
+export function Form({
+  customMethods,
+  children,
+  schema,
+  values,
+  onSubmit,
+}: FormProps) {
+  const localMethods = useForm({
     resolver: schema ? zodResolver(schema) : undefined,
     mode: "onBlur",
     values,
   });
+  const methods = customMethods ?? localMethods;
 
   function handleSubmit(values: any) {
     onSubmit(values, {
